8b3dc9d6767ea0fa06eab6ac01fbd172e3b1ddf3
Aye Swe <ayeswe@MacBook-Pro.local>
2018-11-08 21:11:46 -0800

    A simple java program for jenkins

---
+++ FavoriteFood.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Jenkins;
+
+/**
+ * @author ayeswe
+ *
+ */
+public class FavoriteFood {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Hello Hello from Jenkin.");
+
+		System.out.println("GOOD BYE");
+			
+	}
+
+}
